0c8c4b4e394ad55213d5c19b45b0c3dd699756b9
aswin-kevin <aswinvenkat60@gmail.com>
2024-12-06 04:10:26 +0530

    grpc engine v1

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net"
+
+	"github.com/aswin-kevin/nuclei-grpc/pkg/server"
+	pb "github.com/aswin-kevin/nuclei-grpc/pkg/service"
 
 	nuclei "github.com/projectdiscovery/nuclei/v3/lib"
 	"github.com/projectdiscovery/nuclei/v3/pkg/output"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-func main() {
+const listenAddress = "localhost:8555"
+
+func main1() {
 	ctx := context.Background()
 
 	// Create nuclei engine with options
@@ -27,13 +36,6 @@ func main() {
 
 	defer ne.Close()
 
-	// Set the templates directory to the user's home directory
-	// homeDir, err := os.UserHomeDir()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// templatesDir := filepath.Join(homeDir, "nuclei-templates")
-
 	// Load targets and optionally probe non-http/https targets
 	ne.LoadTargets([]string{"https://securin.io"}, false)
 
@@ -49,4 +51,19 @@ func main() {
 	}
 
 	fmt.Println("Execution completed")
+}
+
+func main() {
+	listener, err := net.Listen("tcp", listenAddress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Println("Started nuclei-api server on:", listenAddress)
+
+	s := grpc.NewServer()
+	pb.RegisterNucleiApiServer(s, &server.Server{})
+	reflection.Register(s)
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
